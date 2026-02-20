8bbfadf8a0bc9bb6b8b9f3e84ff9daf40ba53f84
Antonio Lazzarini Sola <Antonio.Lazzarini-Sola@student.uibk.ac.at>
2026-02-20 23:02:45 +0100

    format change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:match_point/app/app.dart'; 
+import 'package:match_point/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
