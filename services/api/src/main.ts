91e1d2ecbdd18738579c1160cb8d51859b383e5e
Antonio Lazzarini Sola <Antonio.Lazzarini-Sola@student.uibk.ac.at>
2026-02-23 12:55:15 +0100

    enable CORS

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -4,6 +4,16 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:59847',
+      'http://localhost:3000',
+      'http://127.0.0.1:59847',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
