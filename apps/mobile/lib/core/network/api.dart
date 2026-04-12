1a7ed241fdbe1f2b9821ba0d5676c2fe8a638034
Antonio Lazzarini Sola <Antonio.Lazzarini-Sola@student.uibk.ac.at>
2026-04-12 23:11:35 +0200

    esto es lo que te he comentado por whatsapp

--- apps/mobile/lib/core/network/api.dart
+++ apps/mobile/lib/core/network/api.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'api_client.dart';
 
 class Api {
   static final ApiClient client = ApiClient(baseUrl: _baseUrl);
 
-  // Para Flutter Web + backend local
-  // static const String _baseUrl = 'http://localhost:3000';
-
-  // Si luego pruebas en Android emulator, cambia a:
-  static const String _baseUrl = 'http://10.0.2.2:3000';
+  static String get _baseUrl {
+    // por lo que he entendido, mira la plataforma que quiere correr la app y entonces elige qué direccion usar
+    if (kIsWeb) return 'http://localhost:3000';
+    return 'http://10.0.2.2:3000';
+  }
+  // por ejemplo, flutter run -d chrome hace que kIsWeb = true entonces usa ese, pero si escribes flutter run -d android == false,
+  // o flutter run -d <id-del-emulador-android> → kIsWeb == false
 }
